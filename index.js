b31ba74129b1a977cabea063939b9635e7a88935
ismo24 <ismaelkoura@gmail.com>
2024-04-12 12:05:21 +0000

    final 1

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(express.static('public'));
     const payID=req.query.payID?req.query.payID:""
     const codeOTP=req.query.codeOTP?req.query.codeOTP:""
     const serviceCode=req.query.serviceCode?req.query.serviceCode:""
-    // res.send(`Informations payId: ${payID} codeOTP:${codeOTP} serviceCODE ${serviceCode}`)
+    const clientNumber=req.query.clientNumber?req.query.clientNumber:""
+    
    
 
     const url = 'https://apidist.gutouch.net/apidist/sec/touchpayapi/KARBO2991/transaction?loginAgent=56740054&passwordAgent=tPf7tZwPbv';
@@ -36,10 +37,10 @@ app.use(express.static('public'));
               destinataire: '54791752',
               otp: codeOTP
             },
-            amount: 100,
+            amount: 2000,
             callback: 'https://karborotech.net',
-            recipientNumber: '74102140',
-            serviceCode: 'BF_PAIEMENTMARCHAND_OM_TP'
+            recipientNumber: clientNumber,
+            serviceCode: serviceCode
           }
 
    
@@ -52,29 +53,15 @@ app.use(express.static('public'));
               'User-Agent': 'insomnia/2023.5.8'
             }
           });
-          // console.log("response",response)
-          const myData={
-            "message 1":req.query,
-            "message 2":"request is successfull",
-            "message 3":response.data,
-            
-          }
+         
       
-          res.json(myData)
+          res.json(response.data)
       
         
     } catch (error) {
-        // Handle errors, such as if the external API is down
-    // console.log("error",error)
-    // res.status(500).send('An error occurred while fetching data.',error,'queryInfos:',req.query);
-    const myData={
-      "message 1":req.query,
-      "message 2":"request failed",
-      "message 3":error,
-      
-    }
+   
 
-    res.json(myData)
+    res.json(error)
         
     }
     
