f4b038b21effcae15da0140702be998a9b15d9a7
ismo24 <ismaelkoura@gmail.com>
2024-04-13 11:55:20 +0000

    new start

--- index.js
+++ index.js
@@ -20,15 +20,14 @@ app.use(express.static('public'));
     const payID=req.query.payID?req.query.payID:""
     const codeOTP=req.query.codeOTP?req.query.codeOTP:""
     const serviceCode=req.query.serviceCode?req.query.serviceCode:""
-    const clientNumber=req.query.clientNumber?req.query.clientNumber:""
-    
+    // res.send(`Informations payId: ${payID} codeOTP:${codeOTP} serviceCODE ${serviceCode}`)
    
 
     const url = 'https://apidist.gutouch.net/apidist/sec/touchpayapi/KARBO2991/transaction?loginAgent=56740054&passwordAgent=tPf7tZwPbv';
     const authorizationToken = 'Basic NzVlMTRkMTc3MDZhOTAyN2RmODgzN2M1ZWE4YWZhNTQyMzQwNmNlYTI0NDkyZGMxOWQxM2ZhYjY3OGIxM2ExYjozNTQ3NmJmN2ZhNTQyZDQ1MzViZWUxMjNmMzQ1OGM2NTQ5NDcxY2UxYTYzZTFjN2U2NmEwYmQ0MmIzOTM2MTE5';
 
    
-    const  orangeData={
+    const  data={
             idFromClient: payID,
             additionnalInfos: {
               recipientEmail: 'JUNIOR@hubsocial.org',
@@ -39,26 +38,10 @@ app.use(express.static('public'));
             },
             amount: 2000,
             callback: 'https://karborotech.net',
-            recipientNumber: clientNumber,
-            serviceCode: serviceCode
+            recipientNumber: '54791752',
+            serviceCode: 'BF_PAIEMENTMARCHAND_OM_TP'
           }
 
-     const  MoovData={
-            idFromClient: payID,
-            additionnalInfos: {
-              recipientEmail: 'JUNIOR@hubsocial.org',
-            recipientFirstName: 'Moustapha',
-            recipientLastName: 'SECK',
-              destinataire: '54791752',
-            },
-            amount: 2000,
-            callback: 'https://karborotech.net',
-            recipientNumber: clientNumber,
-            serviceCode: serviceCode
-          }
-   
-
-          const data=serviceCode=='BF_PAIEMENTMARCHAND_MOBICASH_TP'?MoovData:orangeData
    
     try {
         
@@ -69,15 +52,29 @@ app.use(express.static('public'));
               'User-Agent': 'insomnia/2023.5.8'
             }
           });
-         
+          // console.log("response",response)
+          const myData={
+            "message 1":req.query,
+            "message 2":"request is successfull",
+            "message 3":response.data,
+            
+          }
       
-          res.json(response.data)
+          res.json(myData)
       
         
     } catch (error) {
-   
+        // Handle errors, such as if the external API is down
+    // console.log("error",error)
+    // res.status(500).send('An error occurred while fetching data.',error,'queryInfos:',req.query);
+    const myData={
+      "message 1":req.query,
+      "message 2":"request failed",
+      "message 3":error,
+      
+    }
 
-    res.json(error)
+    res.json(myData)
         
     }
     
